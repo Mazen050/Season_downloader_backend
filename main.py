2410d5964ad04e6f2e64a6d010450695d9c20cf0
Mzn <71020170+Mazen050@users.noreply.github.com>
2024-10-12 00:23:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/',methods=['GET','POST'])
-#def index():
-#    if request.method == 'POST':
-#        data = request.get_json()
-#        Type = data.get('Type')
-#        link = data.get('link')
-#        print(link)
-#        if "ak.sv" in link:
-#            akwam = ak()
-#            data = akwam.getDLL(link)
-#        elif "wecima" in link:
-#            data = wecimascraper(link)
-#        elif "egyrbyeteuh" or "egydead" in link:
-#            data = egydead_download(link)
-#        return jsonify(data)
-#    return 'Hello from Flask!'
+def index():
+   if request.method == 'POST':
+       data = request.get_json()
+       Type = data.get('Type')
+       link = data.get('link')
+       print(link)
+       if "ak.sv" in link:
+           akwam = ak()
+           data = akwam.getDLL(link)
+       # elif "wecima" in link:
+       #     data = wecimascraper(link)
+       elif "egyrbyeteuh" or "egydead" in link:
+           data = egydead_download(link)
+       return jsonify(data)
+   return 'Hello from Flask!'
 
 @app.route('/search',methods=['GET','POST'])
 def search():
