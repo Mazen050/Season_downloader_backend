df68ee9e8a60a2e0bf8713022a05abe6f3f9a56f
Mazen Kamal <71020170+Mazen050@users.noreply.github.com>
2025-04-23 21:22:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
-from types import MethodType
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from sanic import Sanic
+from sanic.response import json
 from asyncakwam import akwamscrape as ak
-#import asyncplaywright as egy
 from scrapewecima import wecimatitle, wecimaimage, wecimasearch, wecimaseasons
 from fasterwecima import wecimascraper
 from arabseed_async import arabseed_download, searcharab
-# from egydead import egydead_download
 
-app = Flask(__name__)
-CORS(app)
+app = Sanic("SeasonDownloader")
 
-@app.route('/',methods=['GET','POST'])
-def index():
-   if request.method == 'POST':
-       data = request.get_json()
-       Type = data.get('Type')
-       link = data.get('link')
-       print(link)
-       if Type == "akwam":
-           akwam = ak()
-           data = akwam.getDLL(link)
-       elif Type == "wecima":
+@app.route('/',methods = ["GET","POST"])
+def index(request):
+    if request.method == 'POST':
+        data = request.get_json()
+        Type = data.get('Type')
+        link = data.get('link')
+        print(link)
+        if Type == "akwam":
+            akwam = ak()
+            data = akwam.getDLL(link)
+        elif Type == "wecima":
             data = wecimascraper(link)
-       elif Type == "arabseed":
+        elif Type == "arabseed":
             data = arabseed_download(link)
 
-       return jsonify(data)
-   return 'Hello from Flask!'
+        return json(data)
+    return 'Hello from Flask!'
 
 @app.route('/search/wecima',methods=['GET','POST'])
-def searchwecima():
+def searchwecima(request):
     if request.method == 'POST':
         data = request.get_json()
         link = data.get('link')
         data = wecimaseasons(link)
         print(data)
-        return jsonify(data)
+        return json(data)
     return 'Hello from Flask!'
 
 
 @app.route('/search',methods=['GET','POST'])
-def search():
+def search(request):
     if request.method == 'POST':
         data = request.get_json()
         text = data.get('text')
@@ -55,8 +51,6 @@ def search():
         elif type=="arabseed":
             data = searcharab(text)
         print(data)
-        return jsonify(data)
+        return json(data)
     return 'Hello from Flask!'
 
-if __name__ == '__main__':
-  app.run(debug=True,port=5500)
